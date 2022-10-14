04cf5f930841ca01d2480da303238a93494ab903
Victor <71389732+Vitkof@users.noreply.github.com>
2022-10-14 15:58:31 +0300

    a fully working script. +options
    1) sending messages only to users with photos (>10 kb)
    2) it is possible to send messages only to users who are learning your language

--- speaky-com-1st-message-for-people.js
+++ speaky-com-1st-message-for-people.js
@@ -1,5 +1,11 @@
-var countOfPagesScrolled = 3;
+var countOfPagesScrolled = 10;
 var contactsNum = 0;
+// specify your native language
+var myLang = "Russian";
+// is 'hi' only to those who learn your language?
+// if NOT, then change to false instead of true
+var checkPersonIsLearningMyLang = true;
+
 
 async function pageScrollDown(height, countOfPagesScrolled){
 
@@ -17,8 +23,6 @@ async function pageScrollDown(height, countOfPagesScrolled){
         }else
             return sendRequest();
     }, 2000)
-
-
 }
 
 
@@ -27,34 +31,76 @@ function sleep(ms) {
 }
 
 
+async function sendHiMessage(el){
+    contactsNum++;
+    let name = document.getElementsByClassName('identity-name is-title-1')[0]
+    .outerText.slice(0,-3);
+    let id = el.hash.slice(10);
+    console.log(name);
+
+    location.href = `https://www.speaky.com/app/#/messaging?userId=${id}`;
+    await sleep(2000);
+
+    var nocontent = document.getElementsByClassName('chat-not-started-msg');
+    var txtarea= document.getElementsByTagName('textarea')[0];
+
+    if(nocontent){
+        
+        txtarea.focus();
+        txtarea.value = `Hi, ${name}.
+        where does your name come from?`;
+
+        var send = document.querySelector('[aria-label="Send"]');
+        send.click();
+
+        await sleep(1000);
+        txtarea.value = "it's very interesting";
+        send.click();
+    }
+}
+
 async function elementProfileClick(el){
-
         el.click();
-        await sleep(3000);
-        var btnSendMessage = document.getElementsByClassName('altissia-main-button');
+        await sleep(2000);
+        
+        if(checkPersonIsLearningMyLang == true){
+            var lgNames = document.querySelector('.studylg-list')
+                                .querySelectorAll('.lg-name');
 
-        contactsNum++;
-        var name = document.getElementsByClassName('identity-name is-title-1');
-        let id = el.hash.slice(10);
-
-        console.log(name.innerText + contactsNum);
-
-        location.href = `https://www.speaky.com/app/#/messaging?userId=${id}`;
+            lgNames.forEach(function(lg){
+                if(lg.innerText === myLang){ 
+                    sendHiMessage(el);
+                }
+            });
+        }
+        else sendHiMessage(el);
 }
 
 
 async function sendRequest(){
+    const elements = Array.from(
+	    document.getElementsByClassName('new-card community-user-card'));
 
-    const elements = Array.from(document.getElementsByClassName('new-card community-user-card'));
-
-    for (var i = 0; i < elements.length; i++) {
-        var isImg = elements[i].getElementsByTagName('img').length;
-        if(isImg){
-            elementProfileClick(elements[i]);
+    for (let el of elements){
+        var img = el.querySelector('img.user-picture');
+	    
+        if(img){
+            var blob = null;
+            var xhr = new XMLHttpRequest(); 
+            xhr.open('GET', img.currentSrc, true); 
+            xhr.responseType = 'blob';
+            xhr.onload = function() 
+            {
+                blob = xhr.response;
+                if(blob.size > 10000){
+                    elementProfileClick(el);
+                };
+            }
+            xhr.send();
             await sleep(6000);
         }
     }
-
+	
 	console.log('Just saw HI contacts: ' + contactsNum);
 }
 
